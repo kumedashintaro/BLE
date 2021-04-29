12841539d6a7000ba20932d667a56e2c07268547
Kumeda Shintaro <pii.k2.bys@gmail.com>
2021-04-29 18:39:51 +0900

    scan実装

--- app/src/main/java/com/kumeda/ble/MainActivity.kt
+++ app/src/main/java/com/kumeda/ble/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.kumeda.ble
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private val bluetoothLeScanner: BluetoothLeScanner? = bluetoothAdapter?.bluetoothLeScanner
+    private var scanning = false
+    private val handler = Handler()
+
+    private val REQUEST_ENABLE_BT = 1
+    private val SCAN_PERIOD: Long = 10000
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.bluetooth_check_button).setOnClickListener {
+            if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
+        }
+
+        findViewById<Button>(R.id.scan_button).setOnClickListener {
+            scanLeDevice()
+        }
+    }
+
+    private fun scanLeDevice() {
+        bluetoothLeScanner?.let { scanner ->
+            if (!scanning) { // Stops scanning after a pre-defined scan period.
+                handler.postDelayed({
+                    scanning = false
+                    scanner.stopScan(leScanCallback)
+                    println("stopScan")
+                }, SCAN_PERIOD)
+                scanning = true
+                scanner.startScan(leScanCallback)
+                println("startScan")
+            } else {
+                scanning = false
+                scanner.stopScan(leScanCallback)
+                println("stopScan")
+            }
+        }
+    }
+
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+
+            //nameをログで出力する。nullだった場合No Name
+            Log.d("result.device.name", result.device.name ?: "No Name")
+        }
     }
 }
